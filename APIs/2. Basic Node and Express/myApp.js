5f80c538f3c937511af58dcc96899b04be4f4055
Jumanazar <jimmy@shinbo.kr>
2021-09-28 10:44:15 +0900

    Serving static assets was added.

--- APIs/2. Basic Node and Express/myApp.js
+++ APIs/2. Basic Node and Express/myApp.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
-var absolutePath = __dirname + "/views/index.html";
+var indexFilePath = __dirname + "/views/index.html";
+var assetsPath = __dirname + "/public";
 
 app.get("/", function(req, res){
     //res.send('Hello Express');
-    res.sendFile(absolutePath);
+    res.sendFile(indexFilePath);
 });
+
+
+app.use("/public", express.static(assetsPath));
 
 
 
