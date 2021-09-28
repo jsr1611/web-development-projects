363e75d60b0dba66d2198724d88b218a7daff034
Jumanazar <jimmy@shinbo.kr>
2021-09-28 17:05:29 +0900

    Added mongdoDB connection via a dotEnv file and mongoose

--- APIs/3. MongoDB and Mongoose/myApp.js
+++ APIs/3. MongoDB and Mongoose/myApp.js
@@ -1,6 +1,8 @@
+const mongoose = require("mongoose");
 require('dotenv').config();
 
-
+//console.log("db con string: " + process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
 
 const createAndSavePerson = (done) => {
