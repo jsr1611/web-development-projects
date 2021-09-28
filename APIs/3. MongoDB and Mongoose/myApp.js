7fcf5c1be46beec66396c484176593c449585295
Jumanazar <jimmy@shinbo.kr>
2021-09-28 17:34:52 +0900

    Schema and Model were created.

--- APIs/3. MongoDB and Mongoose/myApp.js
+++ APIs/3. MongoDB and Mongoose/myApp.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-//console.log("db con string: " + process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const { Schema } = mongoose;
+
+const personSchema = new Schema({
+  name : String,
+  age :  Number,
+  favoriteFoods : [String]
+});
 let Person;
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
