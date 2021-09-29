5f6006d0ba494aea06cfa1324ce17f95aff66be9
Jumanazar <jimmy@shinbo.kr>
2021-09-29 17:38:41 +0900

    Finished the removeManyPeople method.

--- APIs/3. MongoDB and Mongoose/myApp.js
+++ APIs/3. MongoDB and Mongoose/myApp.js
@@ -100,10 +100,13 @@ const removeById = (personId, done) => {
   });
 };
 
+//Search database and remove many
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name:nameToRemove}, function(err, people){
+    if(err) return console.error(err);
+    done(null, people);
+  });
 };
 
 const queryChain = (done) => {
