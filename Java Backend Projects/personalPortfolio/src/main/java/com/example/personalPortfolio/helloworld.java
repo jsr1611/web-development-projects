b0bce14b350ff49c17f8f654501ac56a4d278ce0
Jumanazar <jsr1611@outlook.com>
2021-11-15 12:44:54 +0900

    Solution was updated.

---
+++ Java Backend Projects/personalPortfolio/src/main/java/com/example/personalPortfolio/helloworld.java
@@ -0,0 +1,14 @@
+package com.example.personalPortfolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloworld {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome to my personal portfolio project!" +
+                "Here, I hope to present the skills and technologies I learned about Java Spring Framework as much as possible.";
+    }
+}
